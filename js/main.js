e12e45b934859b1ae3be04f5e019910d732091aa
Nietzsche <thomasalwyndavis@gmail.com>
2012-06-25 18:57:59 +1000

    ooo server

--- js/main.js
+++ js/main.js
@@ -11,13 +11,11 @@ require.config({
 
     // Require.js plugins
     text: 'libs/require/text',
-    order: 'libs/require/order',
 
     // Just a short cut so we can put our html outside the js dir
     // When you have HTML/CSS designers this aids in keeping them out of the js directory
     templates: '../templates'
-  },
-	urlArgs: "bust=" +  (new Date()).getTime()
+  }
 
 });
 
