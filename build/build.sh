b276f0ee3022483790a6984ec0a19f5e19c6f6f6
Nietzsche <thomasalwyndavis@gmail.com>
2012-06-25 19:24:55 +1000

    ud

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 rm -rf output
 node r.js -o build.js
 node r.js -o cssIn=../css/styles.css out=output/css/styles.css
-
+mkdir output/img
+cp -r ../img output
+cp -r ../css output/css
 cp ../index.html output/index.html
 sed -i 's/js\/libs\/require\/require.js/http:\/\/requirejs.org\/docs\/release\/1.0.5\/minified\/require.js/g' output/index.html
