aa81e865fed72b01a120b0a7e73560a266cfb5e4
root <root@li479-155.members.linode.com>
2012-06-26 13:12:10 +0000

    fixed css imgs

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,6 @@ node r.js -o build.js
 node r.js -o cssIn=../css/styles.css out=output/css/styles.css
 mkdir output/img
 cp -r ../img output
-cp -r ../css output/css
+cp -r ../css/* output/css
 cp ../index.html output/index.html
 sed -i 's/js\/libs\/require\/require.js/http:\/\/requirejs.org\/docs\/release\/1.0.5\/minified\/require.js/g' output/index.html
