8797c241d713535cfe58fef91eb2e5bcf9848da5
Nietzsche <thomasalwyndavis@gmail.com>
2012-07-29 17:02:34 +1000

    Ooo lalalal

---
+++ js/views/forms/method.js
@@ -0,0 +1,47 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'bootstrap',
+  'vm',
+  'models/session',
+  'text!templates/methods/edit.html',
+  'models/method'
+  ], function($, _, Backbone, Vm, bootstrap, Session, methodEditTemplate, MethodModel){
+  var ResourceForm = Backbone.View.extend({
+    el: 'body',
+    initialize: function () {
+      var that = this;
+      this.method = this.options.method;
+      if(typeof this.options.method === 'undefined') {
+        this.method = new MethodModel({
+          username: this.options.username,
+          version: this.options.version,
+          api: this.options.api,
+          resourceId: this.options.resourceId
+        });
+      }
+    },
+    events: {
+      'submit .js-new-method-form': 'saveMethod'
+    },
+    saveMethod: function (ev) {
+      var that = this;
+      var methodData = $(ev.currentTarget).serializeObject();
+      console.log(this.method);
+      this.method.save(methodData, {
+        success: function () {
+          $('#js-new-method-modal').modal('hide');
+        }
+      });
+      return false;
+    },
+    render: function () {
+      var methodTypes = ['POST', 'GET', 'DELETE', 'PUT'];
+      $('body').append(_.template(methodEditTemplate, {methodTypes: methodTypes, method: this.method}));
+      $('#js-new-method-modal').modal('show');
+    
+    }
+  });
+  return ResourceForm;
+});
---
+++ js/views/forms/resource.js
@@ -0,0 +1,44 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'bootstrap',
+  'vm',
+  'models/session',
+  'text!templates/resource/edit.html',
+  'models/resource'
+  ], function($, _, Backbone, Vm, bootstrap, Session, resourceEditTemplate, ResourceModel){
+  var ResourceForm = Backbone.View.extend({
+    el: 'body',
+    initialize: function () {
+      var that = this;
+      this.resource = this.options.resource;
+      if(typeof this.resource === 'undefined') {
+        this.resource = new ResourceModel({
+          username: this.options.username,
+          version: this.options.version,
+          api: this.options.api
+        });
+      }
+    },
+    events: {
+      'submit .js-new-resource-form': 'saveResource'
+    },
+    saveResource: function (ev) {
+      var resourceData = $(ev.currentTarget).serializeObject();
+      console.log(this.resource);
+      this.resource.save(resourceData, {
+        success: function () {
+          console.log('oooo', arguments);
+        }
+      });
+      return false;
+    },
+    render: function () {
+      $('body').append(_.template(resourceEditTemplate, {resource: this.resource}));
+      $('#js-new-resource-modal').modal('show');
+    
+    }
+  });
+  return ResourceForm;
+});
