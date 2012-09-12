85edf1821aadf6458d38d785d8fabb129daa8a4c
Thomas Davis <thomasalwyndavis@gmail.com>
2012-09-12 02:28:05 +0000

    added an api list

--- js/views/browse/page.js
+++ js/views/browse/page.js
@@ -3,8 +3,10 @@ define([
   'underscore',
   'backbone',
   'models/session',
-  'text!templates/browse/page.html'
-], function($, _, Backbone, Session, browseTemplate){
+  'text!templates/browse/page.html',
+  'collections/apis',
+  'text!templates/browse/list.html'
+], function($, _, Backbone, Session, browseTemplate, Apis, apiListTemplate){
   var BrowseView = Backbone.View.extend({
     el: '.page',
     initialize: function () {
@@ -12,6 +14,12 @@ define([
     },
     render: function () {
       this.$el.html(_.template(browseTemplate, {}));
+      var apis = new Apis();
+      apis.fetch({
+        success: function (apis) {
+          $('.api-list').html(_.template(apiListTemplate, {apis: apis.models, _:_}));
+        }
+      })
     }
   });
   return BrowseView;
