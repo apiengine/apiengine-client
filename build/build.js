ffd3bfc527e6a8b41bc09a022c175c59034b7600
Thomas Davis <thomasalwyndavis@gmail.com>
2012-10-17 12:34:17 +0000

    test

--- build/build.js
+++ build/build.js
@@ -34,13 +34,36 @@ fs.writeFileSync('output/index.html', index);
     cssIn: rootPath + '/css/styles.css',
     out: outputFolder + '/css/styles.css'
 });
+
+var path = require('path');
+
+function cssIncImages(cssFile) {
+  var imgRegex = /url\s?\(['"]?(.*?)(?=['"]?\))/gi;
+  var css = fs.readFileSync(cssFile, 'utf-8');
+  while (match = imgRegex.exec(css)) {
+    var imgPath = path.join(path.dirname(cssFile), match[1]);
+    try {
+      var img = fs.readFileSync(imgPath, 'base64');
+      var ext = imgPath.substr(imgPath.lastIndexOf('.') + 1);
+      css = css.replace(match[1], 'data:image/' + ext + ';base64,' + img);
+    } catch (err) {
+      console.log('Image not found (%s).', imgPath);
+    }
+  }
+   fs.writeFileSync(cssFile, css, 'utf-8'); // you can overwrite the original file with this line
+  return css;
+}
+
 	fs.copy(rootPath + '/googleaa49fe030680ef6c.html', 'output/googleaa49fe030680ef6c.html', function (){
 	fs.copy(rootPath + '/.htaccess', 'output/.htaccess', function (){
  	fs.copy(rootPath + '/favicon.ico', 'output/favicon.ico', function (){
     fs.copy(rootPath + '/css/img', outputFolder +'/css/img', function () {
 	 	fs.copy(rootPath + '/img', outputFolder +'/img', function () {
 			fs.copy(rootPath + '/css/Aller_Std_Rg.ttf', outputFolder +'/css/Aller_Std_Rg.ttf', function () {
-				var endTime = (Date.now() - startTime) / 1000;
+				
+cssIncImages(outputFolder + '/css/styles.css');
+
+        var endTime = (Date.now() - startTime) / 1000;
 
       });
 	 		});
