0fe1515489256f9c3e499f08d25840b07a9749ff
Richard Barrett <ricbarrett@gmail.com>
2012-11-12 15:42:45 +1000

    animations

--- js/views/home/page.js
+++ js/views/home/page.js
@@ -25,6 +25,10 @@ define([
       $('.top-bar-menu li a.home-button').addClass('active');
       if(Session.get('auth')){
         this.$el.html(_.template(homeTemplate, {username: Session.get('login')}));
+
+
+
+
         var apisList = new ApisList({is_public: false, el: '.private-container'});
         apisList.render();
       } else {
@@ -32,6 +36,294 @@ define([
         var authView = new AuthView();
         authView.render();
       }
+
+
+
+
+jQuery.easing.def = "easeInBounce";
+
+      var firstslide = {
+        onEnter: function (complete) {
+                var stage = new swiffy.Stage(document.getElementById('swiffycontainer'),
+                                   swiffyobject);
+                stage.start();
+
+            $('#swiffycontainer').css({
+              right: "-500px",
+              opacity: 1
+            })
+           $('.slide1 .heading-container').css({
+              opacity: 1,
+              top: "-300px"
+            })
+           $('.slide1 .tag').css({
+              opacity: 1,
+              top: "300px"
+            })
+          console.log('First slide enters');
+              $('#swiffycontainer').animate({
+                "right": '0',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad',
+                complete:complete
+              });
+
+
+              $('.slide1 .heading-container').animate({
+                "top": '70px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide1 .tag').animate({
+                "top": '190px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+        },
+        onLeave: function () {
+          console.log('First slide leaves');
+
+         $('#swiffycontainer').animate({
+           
+            opacity: 0,
+          }, {
+                duration: 500,
+                easing: 'easeOutQuad',
+                complete: function (){
+               $('#swiffycontainer').empty();
+
+                }
+              });
+
+
+
+         $('.slide1 .heading-container').animate({
+              
+                 opacity: 0,
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide1 .tag').animate({
+               
+                 opacity: 0
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+        }
+
+      }
+      var secondslide = {
+      onEnter: function (complete) {
+               var stage1 = new swiffy.Stage(document.getElementById('swiffycontainer2'),
+                                   swiffyobject2);
+               stage1.start();
+        $('#swiffycontainer2').css({
+              right: "-500px",
+              opacity: 1
+            })
+           $('.slide2 .heading-container').css({
+              opacity: 1,
+              top: "-300px"
+            })
+           $('.slide2 .tag').css({
+              opacity: 1,
+              top: "300px"
+            })
+          console.log('First slide enters');
+              $('#swiffycontainer2').animate({
+                "right": '0',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad',
+                complete:complete
+              });
+
+
+              $('.slide2 .heading-container').animate({
+                "top": '70px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide2 .tag').animate({
+                "top": '190px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+        },
+        onLeave: function () {
+          console.log('First slide leaves');
+
+         $('#swiffycontainer2').animate({
+            opacity: 0,
+          }, {
+                duration: 500,
+                easing: 'easeOutQuad',
+                complete: function (){
+               $('#swiffycontainer2').empty();
+
+                }
+              });
+
+
+
+         $('.slide2 .heading-container').animate({
+                 opacity: 0,
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide2 .tag').animate({
+                opacity: 0,
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+        }
+      }
+      var thirdslide = {
+ onEnter: function (complete) {
+               var stage3 = new swiffy.Stage(document.getElementById('swiffycontainer3'),
+                                   swiffyobject3);
+               stage3.start();
+        $('#swiffycontainer3').css({
+              right: "-500px",
+              opacity: 1
+            })
+           $('.slide3 .heading-container').css({
+              opacity: 1,
+              top: "-300px"
+            })
+           $('.slide3 .tag').css({
+              opacity: 1,
+              top: "300px"
+            })
+          console.log('First slide enters');
+              $('#swiffycontainer3').animate({
+                "right": '0',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad',
+                complete:complete
+              });
+
+
+              $('.slide3 .heading-container').animate({
+                "top": '70px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide3 .tag').animate({
+                "top": '190px',
+              }, {
+                duration: 750,
+                easing: 'easeOutQuad'
+              });
+
+
+        },
+        onLeave: function () {
+          console.log('First slide leaves');
+
+         $('#swiffycontainer3').animate({
+            opacity: 0,
+          }, {
+                duration: 500,
+                easing: 'easeOutQuad',
+                complete: function (){
+               $('#swiffycontainer3').empty();
+
+                }
+              });
+
+
+
+         $('.slide3 .heading-container').animate({
+                 opacity: 0,
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+
+              $('.slide3 .tag').animate({
+                opacity: 0,
+              }, {
+                duration: 500,
+                easing: 'easeOutQuad'
+              });
+
+
+        }
+      }
+
+      var slides = [firstslide, secondslide, thirdslide];
+
+
+      var Slider = function () {
+        this.slides = [];
+        this.currentSlide = 0;
+      }
+      Slider.prototype.complete = function () {
+        var that = this;
+        setTimeout(function () {
+          var oldIndex = that.currentSlide;
+          if(that.currentSlide + 1 === that.slides.length) {
+            that.currentSlide = 0;
+          } else {
+            that.currentSlide = that.currentSlide + 1;
+          }
+          that.changeSlide(oldIndex, that.currentSlide)
+        }, 7000);
+      };
+      Slider.prototype.changeSlide = function (lastSlideIndex, newSlideIndex) {
+        var lastSlide = slides[lastSlideIndex];
+        var newSlide = slides[newSlideIndex];
+        if(lastSlide) {
+          lastSlide.onLeave();
+        }
+        this.currentSlide = newSlideIndex;
+        newSlide.onEnter(this.complete.bind(this));
+      };
+      Slider.prototype.start = function () {
+        this.changeSlide(null, this.currentSlide);
+
+      }
+      var slider = new Slider();
+      slider.slides = slides;
+      slider.start();
+
 
         var apisList = new ApisList({is_public: true, el: '.public-container'});
         apisList.render();
