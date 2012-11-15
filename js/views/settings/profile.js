784a7d890a5170d04de8da6274399000fa4e67fb
Thomas Davis <thomasalwyndavis@gmail.com>
2012-11-15 12:52:36 +0000

    submit profile

--- js/views/settings/profile.js
+++ js/views/settings/profile.js
@@ -5,15 +5,29 @@ define([
   'router',
   'vm',
   'models/session',
-  'text!templates/settings/profile.html'
-], function($, _, Backbone, Router, Vm, Session, settingTemplate){
+  'text!templates/settings/profile.html',
+  'models/profile'
+], function($, _, Backbone, Router, Vm, Session, settingTemplate, ProfileModel){
   var SettingPage = Backbone.View.extend({
     el: '.settings-page-container',
     initialize: function () {
     },  
     events: {
+      'submit .profile-form': 'updateProfile'
+    },
+    updateProfile: function (ev) {
+      var profileDetails = $(ev.currentTarget).serializeObject();
+      var profileModel = new ProfileModel();
+      profileDetails.id = Session.get('user').login;
+      profileModel.save(profileDetails, {
+        success: function () {
+          console.log('profile', arguments);
+        }
+      })
+      return false;
     },
     render: function (options) {
+      console.log(Session, 'session');
       $('.settings-menu a').removeClass('active');
       $('.settings-menu .profile').addClass('active');
       this.$el.html(settingTemplate);
