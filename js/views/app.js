185766b4cd36ba178e59415bc69b09bb0bab290a
Thomas Davis <thomasalwyndavis@gmail.com>
2012-11-16 05:33:56 +0000

    feedback button coming along

--- js/views/app.js
+++ js/views/app.js
@@ -9,8 +9,9 @@ define([
   'text!templates/layout.html',
   'views/header/account-menu',
   'views/header/header',
-  'views/footer/footer'
-], function($, _, Backbone, extensions, Vm, Events, Session, layoutTemplate, AccountMenu, HeaderView, FooterView){
+  'views/footer/footer',
+  'views/feedback/feedback'
+], function($, _, Backbone, extensions, Vm, Events, Session, layoutTemplate, AccountMenu, HeaderView, FooterView, FeedbackView){
   var AppView = Backbone.View.extend({
     el: 'body',
     initialize: function () {
@@ -77,7 +78,14 @@ define([
 //$.ajax('http://d3gscmgl75g1oq.cloudfront.net/user/thomasdavis/api/ApiEngine/1/resource/8', {
   //success: function () {console.log(arguments);}
 //});
-		} 
+		},
+    events: {
+      'click .js-feedback': 'openFeedback'
+    },
+    openFeedback: function () {
+      var feedbackView = Vm.create(this, 'feedback', FeedbackView, {});
+      feedbackView.render();
+    }
 	});
   return AppView;
 });
