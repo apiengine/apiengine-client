a9bbcb6823de8114c5e481e2c0499e0906df6007
pospi <pospi@spadgos.com>
2013-03-03 14:44:44 +1000

    hookup resource list code properly, read notifications for API overview & adjust DOM to make this possible

--- js/views/resource/list.js
+++ js/views/resource/list.js
@@ -16,7 +16,7 @@ define([
     el: '.resource-list-container',
     initialize: function () {
       var that = this;
-      
+
     },
     events: {
       'click a[data-resource-id]': 'showMethodList',
@@ -32,14 +32,14 @@ define([
       } else {
        var el = $('[data-resource-id=' + this.options.resourceId + ']');
       }
-      if(el.length > 0) { 
+      if(el.length > 0) {
         this.expandMethods($(el).parents('li[data-resource-id]'));
       }
-      
+
       //var methodListView = new MethodsListView({username: that.options.username, api: that.options.api, version: that.options.version, resourceId: that.options.resourceId, method: that.options.method});
-      //methodListView.render();        
+      //methodListView.render();
       var resourcePageView = Vm.create(this, 'resourcepageview', ResourcePageView, that.options);
-      resourcePageView.render();  
+      resourcePageView.render();
     },
     expandMethodsHandler: function (ev) {
         $(ev.currentTarget).text('-');
@@ -47,12 +47,12 @@ define([
         var ele = $(ev.currentTarget).parents('li[data-resource-id]');
         this.expandMethods(ele);
     },
-    expandMethods: function (ele) {   
+    expandMethods: function (ele) {
       var resourceId = $(ele).attr('data-resource-id');
       var el =  $(ele).next('li');
       var methodListView = Vm.create(this, 'methodlist'+this.options.resourceId, MethodsListView, {username: this.options.username, apiname: this.options.apiname, version: this.options.version, resourceId: resourceId, method: this.options.method, el: el});
       methodListView.setElement(el);
-      methodListView.render();    
+      methodListView.render();
     },
     contractMethods: function (ev) {
       $(ev.currentTarget).removeClass('expanded');
@@ -67,16 +67,18 @@ define([
       that.$el.attr('data-api-id', that.options.apiname);
       resources.fetch({
         success: function (collection) {
+          // mark active resource in list if we're viewing one
           if(that.options.resourceId) {
-            
-          collection.get(that.options.resourceId).set({active: 'active'});
+          	collection.get(that.options.resourceId).set({active: 'active'});
           }
           that.$el.html(Mustache.render(resourceListTemplate, {_:_, selectedResource: that.options.resourceId, is_public: that.options.is_public, resources: collection, username: Session.get('login'), location: that.options.location}));
-          if(typeof that.options.resourceId !== 'undefined') { //&& $('.method-list-container').attr('data-resource-id') !== that.options.resourceId) {
-             that.showMethodList();           
-          }
-          //that.showMethodList();  
 
+          // render method list for active resource
+          if(typeof that.options.resourceId !== 'undefined') {
+             that.showMethodList();
+          }
+
+          // read in notification counts
           var notificationTotals = new NTotals();
           notificationTotals.options = {
             api: that.options.apiname,
@@ -84,17 +86,19 @@ define([
             username: that.options.username
           };
           notificationTotals.fetch({
-            success: function (model) {
-              console.log('I dont know what to do', model);
-              //var notifEl = $('.notification[data-resource-id='+model.options.resourceId+']');
-             // notifEl.text(model.get('resource')).show();
+            success: function(model) {
+              // set notifiactions on the top-level API
+              if (model.get('api')) {
+              	var anotifEl = $('ul.api-menu-list[data-api-id='+that.options.apiname+'] .api-notification');
+                anotifEl.text(model.get('api')).show();
+              }
+
+              // set notifications on each resource
               _.each(model.get('resources'), function(method){
-                console.log(method,'wtf is this');
                 if(method.count > 0) {
-                  var anotifEl = $('.resource-notification[data-resource-id='+method.key+']');
+                  var anotifEl = $('li[data-resource-id='+method.key+'] .resource-notification');
                   anotifEl.text(method.count).show();
                 }
-
               });
             }
           });
@@ -105,7 +109,7 @@ define([
         }
       });
 
-      
+
     }
   });
   return ApisPage;
