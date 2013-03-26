ee847e18fbe770f5a3579b87a1c0b99002b8df36
Samson <samson@gmail.com>
2013-03-26 09:15:08 +0000

    updated build

--- build/build.js
+++ build/build.js
@@ -4,26 +4,62 @@ var http = require('http-get');
 var rjs = require('requirejs');
 var knox = require('knox');
 var glob = require("glob")
+var clc = require('cli-color');
 
-console.log(process.argv);
+// CONFIG 
 
-var cloudfront = 'https://d3oqwi49u9bqjg.cloudfront.net';
+var cloudfront_production = 'https://d3oqwi49u9bqjg.cloudfront.net';
+var cloudfront_stage = 'https://d51ivijxlr7mx.cloudfront.net';
+
+// Logging helpers
+
+var types = {
+  default: clc.black,
+  error: clc.red.bold,
+  warn: clc.yellow,
+  heading: clc.blue.bold.underline,
+  label: clc.blue.bold,
+  action: clc.xterm(8).italic
+};
+
+var log = function (message, _type) {
+  var type = _type || types.default;
+  console.log(type(message) + '\n');
+}
+
+// Start build
+
+log('Api Engine Client Builder', types.heading)
+
+
+
 
 if(process.argv[2] === 'stage') {
-  var cloudfront = 'https://d51ivijxlr7mx.cloudfront.net';
+  var cloudfront = cloudfront_stage;
+  log('Staging enviroment selected', types.label)
+} else {
+  var cloudfront = cloudfront_production;
+  log('Production enviroment selected', types.label)
 }
+
 
 
 var version = Date.now();
 var outputFolder = 'output/version/' + version
-console.log('Building client');
+log('Starting build');
 var startTime = Date.now();
+
+log('Create output folders', types.action);
 
 fs.removeSync('output');
 fs.mkdirSync('output');
 fs.mkdirSync('output/version');
 fs.mkdirSync(outputFolder);
+
+
 var rootPath = '..';
+log('Running require.js optimizer', types.action)
+
   rjs.optimize({
     dir: outputFolder + '/js',
     mainConfigFile: rootPath + '/js/main.js',
@@ -60,10 +96,16 @@ var rootPath = '..';
       }
     ]
 });
+log('Optimization finished', types.action)
+log('Editing index.html to contain new enviroment variables', types.action)
+
 var index = fs.readFileSync(rootPath + '/index.html', 'ascii');
 index = index.replace('css/styles.css', cloudfront + '/version/' + version + '/css/styles.css');
 index = index.replace('<base href="/repos/apiengine-client/" />', '');
 index = index.replace(' data-main="js/main"', ' data-main="' + cloudfront + '/version/' + version + '/js/main"');
+
+log('Optimization css with require.js', types.action)
+
 fs.writeFileSync('output/index.html', index);
   rjs.optimize({
     cssIn: rootPath + '/css/styles.css',
@@ -88,6 +130,7 @@ function cssIncImages(cssFile) {
    fs.writeFileSync(cssFile, css, 'utf-8'); // you can overwrite the original file with this line
   return css;
 }
+log('Copying accross more needed files', types.action)
 
   fs.copy(rootPath + '/googleaa49fe030680ef6c.html', 'output/googleaa49fe030680ef6c.html', function (){
   fs.copy(rootPath + '/site.xml', 'output/site.xml', function (){
@@ -96,11 +139,14 @@ function cssIncImages(cssFile) {
     fs.copy(rootPath + '/css/img', outputFolder +'/css/img', function () {
     fs.copy(rootPath + '/img', outputFolder +'/img', function () {
       fs.copy(rootPath + '/css/fonts', outputFolder +'/css/fonts', function () {
+log('Embedding images into CSS', types.action)
         
 cssIncImages(outputFolder + '/css/styles.css');
       fs.copy('output', 'relic', function () {
-        
         var endTime = (Date.now() - startTime) / 1000;
+log('Build finished!', types.heading)
+log('Finished in ' + endTime + ' seconds', types.label)
+        
 });
       });
       });
